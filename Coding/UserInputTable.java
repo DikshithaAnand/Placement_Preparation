093743386dc34a35febb0e78ac9cad2742d7e41b
Dikshitha A <dikshitha.anand7@gmail.com>
2026-07-21 12:42:12 +0530

    Add UserInputTable class for multiplication table

---
+++ Coding/UserInputTable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class UserInputTable {
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Enter the number: ");
+    int num = sc.nextInt();
+    int i = 1;
+    
+    while(i <= 10)
+    {
+        System.out.println(num + " * " + i + " = " + (num*i));
+        i++;
+    }
+    sc.close();
+}
+}
